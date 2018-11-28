9f9a880566643553d310672b6ee2dfdba6a4ff86
John Ding <python@sohu.com>
2018-11-28 19:05:36 +0800

    add oop training

---
+++ oop/student1/student1_test.go
@@ -0,0 +1,13 @@
+package student1
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestStudentSayHi(t *testing.T) {
+	s := Student{}
+	s.Init("John", 25, "cs")
+	s.SayHi()
+	fmt.Printf("name is %s\n", s.name)
+}
